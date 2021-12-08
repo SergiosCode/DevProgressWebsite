782395e423e59f0210898d9f439d47ce06608c65
Sergio Valencia <Streameraudio@yahoo.com>
2021-12-07 22:33:23 -0800

    Finished Contact Us Page

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const modalButton = document.querySelector(".menu");
 
 // modal functionality
 document.querySelector(".menu").addEventListener("click", () => {
-  document.querySelector(".modal").classList.contains("open-modal")
-    ? document.querySelector(".modal").classList.remove("open-modal")
-    : document.querySelector(".modal").classList.add("open-modal");
+  document.querySelector(".modal").classList.contains("open-modal") ? document.querySelector(".modal").classList.remove("open-modal") : document.querySelector(".modal").classList.add("open-modal");
 });
+
+const btn = document.querySelector("#btn");
+const btnText = document.querySelector("#btnText");
+
+btn.onclick = () => {
+  btnText.innerHTML = "Received";
+  btn.classList.add("active");
+};
