c5a0362ba558f6a14ad6f1a7a387faedb972e56a
SergiosCode <streameraudio@yahoo.com>
2022-01-14 22:53:56 -0800

    ..

--- src/script.js
+++ src/script.js
@@ -3,9 +3,19 @@ const modal = document.querySelector(".modal");
 const modalButton = document.querySelector(".menu");
 
 // modal functionality
-document.querySelector(".menu").addEventListener("click", () => {
-  document.querySelector(".modal").classList.contains("open-modal") ? document.querySelector(".modal").classList.remove("open-modal") : document.querySelector(".modal").classList.add("open-modal");
-});
+document
+  .querySelector(".menu")
+  .addEventListener("click", () => {
+    document
+      .querySelector(".modal")
+      .classList.contains("open-modal")
+      ? document
+          .querySelector(".modal")
+          .classList.remove("open-modal")
+      : document
+          .querySelector(".modal")
+          .classList.add("open-modal");
+  });
 
 const btn = document.querySelector("#btn");
 const btnText = document.querySelector("#btnText");
